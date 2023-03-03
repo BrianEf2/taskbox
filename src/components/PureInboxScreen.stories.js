651558d5b5a7dab8551c94c5a7c98f9e2df56e3a
Brian Koudijs <brian@ef2.nl>
2023-03-03 15:30:27 +0100

    Remove interactions

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,8 +1,6 @@
 
 import { app } from '@storybook/vue3';
 
-import { fireEvent, within } from '@storybook/testing-library';
-
 import { createPinia } from 'pinia';
 
 app.use(createPinia());
@@ -28,12 +26,3 @@ export const Default = Template.bind({});
 
 export const Error = Template.bind({});
 Error.args = { error: true };
-
-export const WithInteractions = Template.bind({});
-WithInteractions.play = async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    // Simulates pinning the first task
-    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
-    // Simulates pinning the third task
-    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
-};
